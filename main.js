08059d24ad6e35c68f582947552566c714715315
ZiadGold <79105186+ZiadGold@users.noreply.github.com>
2024-03-10 15:02:53 +0300

    Update main.js

--- main.js
+++ main.js
@@ -80,12 +80,6 @@ async function run(){
     }
     else {
       browser = await puppeteer.launch({
-        args: [
-          "--disable-setuid-sandbox",
-          "--no-sandbox",
-          "--single-process",
-          "--no-zygote",
-        ],
         executablePath:
           process.env.NODE_ENV === "production"
             ? process.env.PUPPETEER_EXECUTABLE_PATH
