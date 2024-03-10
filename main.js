6aa945539658ecf4063d4380abaea2001c10658d
ZiadGold <79105186+ZiadGold@users.noreply.github.com>
2024-03-10 14:58:57 +0300

    Update main.js

--- main.js
+++ main.js
@@ -96,7 +96,7 @@ async function run(){
     const page = await browser.newPage();
 
     // const loginUrl = "https://accounts.google.com/AccountChooser?service=mail&continue=https://google.com&hl=en";
-    const ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36'; 
+    const ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'; 
 
     await page.setUserAgent(ua);
     // await page.goto(loginUrl, { waitUntil: 'networkidle2' });
