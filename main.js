5d83954a50ed4b4149c38000a641960ced959d65
ZiadGold <79105186+ZiadGold@users.noreply.github.com>
2024-03-14 17:15:54 +0300

    Update main.js

--- main.js
+++ main.js
@@ -97,115 +97,110 @@ async function run(){
     }
     
 
-    try {
-      const page = await browser.newPage();
+    const page = await browser.newPage();
+  
+    await page.emulateTimezone('Asia/Riyadh');
+    const response = await page.goto('http://scooterlabs.com/echo.json');
+
+
+
     
-      await page.emulateTimezone('Asia/Riyadh');
-      const response = await page.goto('http://scooterlabs.com/echo.json');
-      console.log(`try: "${await page.evaluate(() => document.title)}"`)
-    }
-    catch(err) {
-      console.log(`catch: "${err.message}" and: "${await page.evaluate(() => document.title)}"`)      
-    }
-    console.log(`out: "${await page.evaluate(() => document.title)}"`)
+    //here
 
+    console.log(await response.json());
 
+    await new Promise(resolve => setTimeout(resolve, 5000 * 120));
 
-    //const response = await page.goto('http://scooterlabs.com/echo.json');
     
-    ////here
 
-    // console.log(await response.json());
+    // const loginUrl = "https://accounts.google.com/AccountChooser?service=mail&continue=https://google.com&hl=en";
+    const ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'; 
 
+    await page.setUserAgent(ua);
+    // await page.goto(loginUrl, { waitUntil: 'networkidle2' });
+    // await new Promise(resolve => setTimeout(resolve, 5000));
+    // await page.type('input[type="email"]', googleUsername, { delay: 50 });
+    // await page.keyboard.press('Enter');
+    // await new Promise(resolve => setTimeout(resolve, 5000));
+    // await page.type('input[type="password"]', googlePassword, { delay: 50 });
+    // await page.keyboard.press('Enter');
 
-    // //const loginUrl = "https://accounts.google.com/AccountChooser?service=mail&continue=https://google.com&hl=en";
-    // const ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'; 
+    // await new Promise(resolve => setTimeout(resolve, 5000 * 60));
 
-    // await page.setUserAgent(ua);
-    // // await page.goto(loginUrl, { waitUntil: 'networkidle2' });
-    // // await new Promise(resolve => setTimeout(resolve, 5000));
-    // // await page.type('input[type="email"]', googleUsername, { delay: 50 });
-    // // await page.keyboard.press('Enter');
-    // // await new Promise(resolve => setTimeout(resolve, 5000));
-    // // await page.type('input[type="password"]', googlePassword, { delay: 50 });
-    // // await page.keyboard.press('Enter');
+    await page.goto("https://example.com/");
+    console.log(await page.evaluate(() => document.title))
+    await page.goto("https://my.viewclass.com/login?lang=en");
+    console.log(await page.evaluate(() => document.title))
+    var username = "20180055";
+    var password = "802330";
+    var account_result = await client.db("viewclass").collection("accounts").findOne({username: username});
+    console.log(account_result)
+    var result
+    if (account_result == null) {
+        console.log("Account not found")
+        // Find the username and password input fields and enter the desired values
+        await page.type('input[name="username"]', username);
+        await page.type('input[name="password"]', password);
+        await new Promise(resolve => setTimeout(resolve, 2000));
 
-    // // await new Promise(resolve => setTimeout(resolve, 5000 * 60));
+        // Switch to reCAPTCHA iframe
+        await page.waitForSelector('iframe[title="reCAPTCHA"]');
+        const frameHandle = await page.$('iframe[title="reCAPTCHA"]');
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        const frame = await frameHandle.contentFrame();
 
-    // await page.goto("https://example.com/");
-    // console.log(await page.evaluate(() => document.title))
-    // await page.goto("https://my.viewclass.com/login?lang=en");
-    // console.log(await page.evaluate(() => document.title))
-    // var username = "20180055";
-    // var password = "802330";
-    // var account_result = await client.db("viewclass").collection("accounts").findOne({username: username});
-    // console.log(account_result)
-    // var result
-    // if (account_result == null) {
-    //     console.log("Account not found")
-    //     // Find the username and password input fields and enter the desired values
-    //     await page.type('input[name="username"]', username);
-    //     await page.type('input[name="password"]', password);
-    //     await new Promise(resolve => setTimeout(resolve, 2000));
-
-    //     // Switch to reCAPTCHA iframe
-    //     await page.waitForSelector('iframe[title="reCAPTCHA"]');
-    //     const frameHandle = await page.$('iframe[title="reCAPTCHA"]');
-    //     await new Promise(resolve => setTimeout(resolve, 2000));
-    //     const frame = await frameHandle.contentFrame();
-
-    //     // Click on reCAPTCHA checkbox
-    //     await frame.waitForSelector('#recaptcha-anchor-label');
-    //     await frame.click('#recaptcha-anchor-label');
-    //     await new Promise(resolve => setTimeout(resolve, 8000));
+        // Click on reCAPTCHA checkbox
+        await frame.waitForSelector('#recaptcha-anchor-label');
+        await frame.click('#recaptcha-anchor-label');
+        await new Promise(resolve => setTimeout(resolve, 8000));
 
 
-    //     var title = await page.evaluate(() => document.title)
-    //     console.log(title)
-    //     if (title == "Home | ViewClass LMS") {
-    //         await page.goto("https://my.viewclass.com/student/subject/homework");
-    //         await new Promise(resolve => setTimeout(resolve, 5000));
-    //         await page.waitForSelector('div.dataTables_scroll');
+        var title = await page.evaluate(() => document.title)
+        console.log(title)
+        if (title == "Home | ViewClass LMS") {
+            await page.goto("https://my.viewclass.com/student/subject/homework");
+            await new Promise(resolve => setTimeout(resolve, 5000));
+            await page.waitForSelector('div.dataTables_scroll');
 
-    //         // Extract the HTML content of the element
-    //         const assignmentsHTML = await page.$eval('div.dataTables_scroll', element => element.innerHTML);
+            // Extract the HTML content of the element
+            const assignmentsHTML = await page.$eval('div.dataTables_scroll', element => element.innerHTML);
           
-    //         // Parse the HTML content to extract assignment data
-    //         const assignments = [];
-    //         const $ = cheerio.load(assignmentsHTML);
-    //         $('tbody tr').each((index, element) => {
-    //           const cells = $(element).find('td');
-    //           const assignment = {
-    //             title: $(cells[0]).text().trim(),
-    //             course: $(cells[1]).text().trim(),
-    //             publish_date: $(cells[2]).text().trim(),
-    //             cutoff_date: $(cells[3]).text().trim(),
-    //             submit: $(cells[4]).text().trim(),
-    //             mark: $(cells[5]).text().trim(),
-    //             correcting: $(cells[6]).text().trim(),
-    //             action: $(cells[7]).find('a').text().trim() || ''
-    //           };
-    //           assignments.push(assignment);
-    //         });
+            // Parse the HTML content to extract assignment data
+            const assignments = [];
+            const $ = cheerio.load(assignmentsHTML);
+            $('tbody tr').each((index, element) => {
+              const cells = $(element).find('td');
+              const assignment = {
+                title: $(cells[0]).text().trim(),
+                course: $(cells[1]).text().trim(),
+                publish_date: $(cells[2]).text().trim(),
+                cutoff_date: $(cells[3]).text().trim(),
+                submit: $(cells[4]).text().trim(),
+                mark: $(cells[5]).text().trim(),
+                correcting: $(cells[6]).text().trim(),
+                action: $(cells[7]).find('a').text().trim() || ''
+              };
+              assignments.push(assignment);
+            });
           
-    //         console.log(assignments);
-    //         await page.goto("https://my.viewclass.com/logout");
+            console.log(assignments);
+            await page.goto("https://my.viewclass.com/logout");
             
-    //         if (await page.evaluate(() => document.title == "تسجيل الدخول | فيوكلاس LMS")) {console.log("logged out successfuly")} 
-    //     }
-    //     else {
-    //         console.log(`Login to ${username} with ${password} was not successful.`)
-    //     }
+            if (await page.evaluate(() => document.title == "تسجيل الدخول | فيوكلاس LMS")) {console.log("logged out successfuly")} 
+        }
+        else {
+            console.log(`Login to ${username} with ${password} was not successful.`)
+        }
         
-    //     //result = await client.db("viewclass").collection("accounts").insertOne({username: username}); 
-    //     //console.log(result)
-    // }
-    // // await page.screenshot({path: "screenshot.png", fullPage: true})
-    // // await page.pdf({path: "screenshot.pdf", format: "A4"})
+        //result = await client.db("viewclass").collection("accounts").insertOne({username: username}); 
+        //console.log(result)
+    }
+    // await page.screenshot({path: "screenshot.png", fullPage: true})
+    // await page.pdf({path: "screenshot.pdf", format: "A4"})
 
-    // // const html = await page.content();
+    // const html = await page.content();
 
-    // // const title = await page.evaluate(() => document.title)
+    // const title = await page.evaluate(() => document.title)
 
     
 
